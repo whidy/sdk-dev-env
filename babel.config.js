19b2769449a8e5a3d5e19238c7546257029e5c48
whidy@whidy.net <whidy@whidy.net>
2019-05-13 11:57:57 +0800

    移除babel/polyfill

--- babel.config.js
+++ babel.config.js
@@ -9,8 +9,19 @@ const presets = [
         firefox: '60',
         chrome: '67',
         safari: '11.1'
+        /**
+         * you can also set browsers here
+         * "browsers": ["last 3 versions", "safari 7"]
+         * relative links:
+         * https://babeljs.io/docs/en/next/babel-preset-env.html
+         * https://github.com/browserslist/browserslist
+         */
       },
       corejs: '3', // should be set
+      /**
+       * https://babeljs.io/docs/en/usage#polyfill
+       * "usage" will practically apply the last optimization mentioned above where you only include the polyfills you need
+       */
       useBuiltIns: 'usage'
     }
   ]
